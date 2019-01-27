c193aee94ae38d8aeebe78f5121f8695cf8fd435
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-01-27 20:20:36 +0530

    Added the grid cells and row

--- src/App.js
+++ src/App.js
@@ -2,27 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+
+function GridCell(){
+	var style = {
+		width:20,
+		height:20,
+		border:'1px solid black',
+		backgroundColor:'red'
+	} 
+	return(
+		<div style={style}></div>
+	);
+
 }
 
-export default App;
+function GridRow(){
+	return(
+		<div>
+			<GridCell/>
+			<GridCell/>
+			<GridCell/>
+		</div>
+	);
+}
+
+export default GridRow;
